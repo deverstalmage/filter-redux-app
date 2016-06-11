cdada17e1afaab586f65b3b1d38e5e1094ea270d
Devers Talmage <devers@tryscratch.com>
2016-06-11 04:01:16 -0400

    build files;

---
+++ webpack.config.prod.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      include: [__dirname + '/src', __dirname + '/data'],
+      loaders: ['react-hot', 'jsx', 'babel']
+    }, {
+      test: /\.scss$/,
+      loaders: ['style', 'css', 'sass']
+    }]
+  },
+  resolve: {
+    root: [__dirname + '/node_modules', __dirname + '/src', __dirname],
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'app.js'
+  },
+};
